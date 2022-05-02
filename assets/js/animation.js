0d0e11655c18f34a43e5a427b9668d4b19818ead
Ralf Herizo <ralflionel120@gmail.com>
2022-05-02 21:26:13 +0300

    reinitialisation de l'evenement du menu burger

--- assets/js/animation.js
+++ assets/js/animation.js
@@ -1,29 +1,49 @@
-toggle = () =>{
-    let x = document.getElementById("collapsibleNavbar");
-    let navbar_close_icon = document.getElementById('navbar-close-icon');
-    let navbar_open_icon = document.getElementById('navbar-open-icon');
+// toggle = () =>{
+//     var x = document.getElementById("collapsibleNavbar");
+//     let navbar_close_icon = document.getElementById('navbar-close-icon');
+//     let navbar_open_icon = document.getElementById('navbar-open-icon');
 
-  if (x.style.height === "230px") {
-    x.style.height = "0px";
-    x.style.overflow = "hidden";
-    navbar_close_icon.style.display = "none";
-    navbar_open_icon.style.display = "block";
-  } else {
-    x.style.height = "230px";
-    navbar_close_icon.style.display = "block";
-    navbar_open_icon.style.display = "none";
-  }
-}
+//     x.style.height = "230px";
+//     if (x.style.height === "230px") {
+//         x.style.height = "0px";
+//         x.style.overflow = "hidden";
+//         navbar_close_icon.style.display = "none";
+//         navbar_open_icon.style.display = "block";
+//     } else {
+        
+//         navbar_close_icon.style.display = "block";
+//         navbar_open_icon.style.display = "none";
+//     }
+// }
 
-let big_content = document.getElementById('big-content');
+// let big_content = document.getElementById('big-content');
 
-big_content.addEventListener('click',()=>{
-    let x = document.getElementById("collapsibleNavbar");
-    let navbar_close_icon = document.getElementById('navbar-close-icon');
-    let navbar_open_icon = document.getElementById('navbar-open-icon');
+// big_content.addEventListener('click',()=>{
+//     let x = document.getElementById("collapsibleNavbar");
+//     let navbar_close_icon = document.getElementById('navbar-close-icon');
+//     let navbar_open_icon = document.getElementById('navbar-open-icon');
     
-    x.style.height = "0px";
-    x.style.overflow = "hidden";
-    navbar_close_icon.style.display = "none";
-    navbar_open_icon.style.display = "block";
-});
+//     x.style.height = "0px";
+//     x.style.overflow = "hidden";
+//     navbar_close_icon.style.display = "none";
+//     navbar_open_icon.style.display = "block";
+// });
+
+
+toggle = ()=>{
+    // document.getElementById('navbar-open-icon').style.display = "none";
+    // document.getElementById('navbar-close-icon').style.display = "block";
+
+    var x = document.getElementById("collapsibleNavbar");
+
+    if (x.style.height === "230px") {
+        x.style.height = "0px";
+        x.style.overflow = "hidden";
+        document.getElementById('navbar-close-icon').style.display = "none";
+        document.getElementById('navbar-open-icon').style.display = "block";
+    } else {
+        document.getElementById('navbar-close-icon').style.display = "block";
+        document.getElementById('navbar-open-icon').style.display = "none";
+        x.style.height = "230px";
+    }
+}
