39f55ca011b7d6d11c5898ede4fcfd08fc0ee9c2
Ralf Herizo <ralflionel120@gmail.com>
2022-05-02 20:27:17 +0300

    edit the burger menu action. Making it work

--- assets/js/animation.js
+++ assets/js/animation.js
@@ -1,11 +1,29 @@
-let navbar = document.getElementById('navbar');
+toggle = () =>{
+    let x = document.getElementById("collapsibleNavbar");
+    let navbar_close_icon = document.getElementById('navbar-close-icon');
+    let navbar_open_icon = document.getElementById('navbar-open-icon');
 
-window.addEventListener('scroll',()=>{
-    if(scrollY > 1){
-        navbar.classList.add('navbar-moov');
-    }
-    else
-    {
-        navbar.classList.remove('navbar-moov');
-    }
+  if (x.style.height === "230px") {
+    x.style.height = "0px";
+    x.style.overflow = "hidden";
+    navbar_close_icon.style.display = "none";
+    navbar_open_icon.style.display = "block";
+  } else {
+    x.style.height = "230px";
+    navbar_close_icon.style.display = "block";
+    navbar_open_icon.style.display = "none";
+  }
+}
+
+let big_content = document.getElementById('big-content');
+
+big_content.addEventListener('click',()=>{
+    let x = document.getElementById("collapsibleNavbar");
+    let navbar_close_icon = document.getElementById('navbar-close-icon');
+    let navbar_open_icon = document.getElementById('navbar-open-icon');
+    
+    x.style.height = "0px";
+    x.style.overflow = "hidden";
+    navbar_close_icon.style.display = "none";
+    navbar_open_icon.style.display = "block";
 });
